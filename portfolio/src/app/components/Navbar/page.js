892a0e7e12d5193ab6b56583d7b0eb6b0be18fa0
Nirmalfageria <nirmal_b221082ee@nitc.ac.in>
2025-01-13 22:12:08 +0530

    .

--- portfolio/src/app/components/Navbar/page.js
+++ portfolio/src/app/components/Navbar/page.js
@@ -3,9 +3,10 @@ import styles from './navbar.module.css'
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <div className={`${styles.container}`}>
+    <nav className="bg-gray-800 py-4 px-4">
+      <div className="container mx-auto my-auto">
         navbar
+       
       </div>
     </nav>
   )
