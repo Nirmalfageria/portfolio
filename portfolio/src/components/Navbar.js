c3d5ea422ea18756ea043af751483f70a267496a
nirmal fageria <nirmalfageria15@gmail.com>
2025-02-25 00:27:59 +0530

    git link ready

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -117,7 +117,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/nirmalfageria/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
